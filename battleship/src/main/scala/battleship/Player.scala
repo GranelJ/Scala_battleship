aa96e7c41ed5481da8d2e9aa052ab2d47bee4cc1
GranelJ <joris.granel@etu.umontpellier.fr>
2018-10-03 15:51:22 +0200

    EDIT : add as_Lost method to Player class with test EDIT : add doc to Player class methods

--- battleship/src/main/scala/battleship/Player.scala
+++ battleship/src/main/scala/battleship/Player.scala
@@ -2,4 +2,12 @@ package battleship
 
 
 case class Player (shipBoard : Board, shotBoard: Board){
+
+  /**
+    * Function to know if a Player as lost
+    * @return : true if the Player doesn't have ship on his board, false otherwise
+    */
+  def as_Lost() : Boolean = {
+      return !this.shipBoard.as_Ship()
+    }
 }
