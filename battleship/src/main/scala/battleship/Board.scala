5dc868232822996d141b1ab9f8f83125dc6443f4
GranelJ <joris.granel@etu.umontpellier.fr>
2018-10-03 15:15:05 +0200

    EDIT : add as_Ship method to Board class with test EDIT : add doc to Board class methods

--- battleship/src/main/scala/battleship/Board.scala
+++ battleship/src/main/scala/battleship/Board.scala
@@ -3,8 +3,13 @@ package battleship
 
 case class Board (grid : List[List[Int]]){
 
-    //Function to check if the ship can be placed on the board, true if it can be placed otherwise false
-    def Check_Ship(ship : Ship) : Boolean = {
+  /**
+    * Function to check if the ship can be placed on the board
+    *
+    * @param ship : the ship to check
+    * @return : true if it can be placed otherwise false
+    */
+  def check_Ship(ship: Ship): Boolean = {
         val ship_size = ship.size()
         //If ship horizontal
         if(ship.orientation){
@@ -13,7 +18,7 @@ case class Board (grid : List[List[Int]]){
             }else{
                 val shipCoord = List.range(ship.x, ship.x + ship.size())
                 //return true if the ship doesn't overpass
-                return !ShipOverpass(ship, shipCoord, false)
+              return !shipOverpass(ship, shipCoord, false)
             }
         //Ship vertical
         }else{
@@ -22,100 +27,156 @@ case class Board (grid : List[List[Int]]){
             }else{
                 val shipCoord = List.range(ship.y, ship.y + ship.size())
                 //return true if the ship doesn't overpass
-                return !ShipOverpass(ship, shipCoord, false)
+              return !shipOverpass(ship, shipCoord, false)
             }
         }
-    }
+  }
 
-    //Function that returns true if a ship overpass on another
-    def ShipOverpass(ship : Ship, shipCoord : List [Int], overPass : Boolean): Boolean = {
-        if(shipCoord.size == 0 || overPass){
+  /**
+    * Function to know of a ship overpass on another
+    *
+    * @param ship      : the Ship to test
+    * @param shipCoord : the List of x coordinates for horizontal ship, the List of y coordinates for vertical ship
+    * @param overPass  : the Boolean to know if the ship overpass
+    * @return : true if a ship overpass on another, false otherwise
+    */
+  def shipOverpass(ship: Ship, shipCoord: List[Int], overPass: Boolean): Boolean = {
+    if (shipCoord.isEmpty || overPass) {
             return overPass
         }else{
             if(ship.orientation){
                 val nOverpass = this.grid(ship.y)(shipCoord.head) > 0
-                ShipOverpass(ship, shipCoord.tail, nOverpass)
+              shipOverpass(ship, shipCoord.tail, nOverpass)
             }else{
                 val nOverpass = this.grid(shipCoord.head)(ship.x) > 0
-                ShipOverpass(ship, shipCoord.tail, nOverpass)
+              shipOverpass(ship, shipCoord.tail, nOverpass)
             }
         }
     }
 
-    /**
-    @tailrec
-    **/
-    //Function to create the list of coordinate that will be added to the grid
-    def Create_ship_list_horizontal(shiplist : List[Int], shipCoord : List[Int], ship : Ship) : List[Int] = {
+  /**
+    * Function to create the List of coordinate that will be added to the grid
+    *
+    * @param shiplist  : the List of Int which will be added in the Board grid
+    * @param shipCoord : the List of x coordinates of the ship to add
+    * @param ship      : the ship to add
+    * @return : a List of Int which have the number of the ship at his x coordinates
+    */
+  def create_Ship_List_Horizontal(shiplist: List[Int], shipCoord: List[Int], ship: Ship): List[Int] = {
         if (shipCoord.size == 0){
             return shiplist
         }else{
             val nshiplist = shiplist.updated(shipCoord.head, ship.number())
             val nshipCoord = shipCoord.tail
-            Create_ship_list_horizontal(nshiplist, nshipCoord, ship)
+          create_Ship_List_Horizontal(nshiplist, nshipCoord, ship)
         }
-    }
+  }
 
-    //Function to place an horizontal ship on the board
-    def Add_ship(ship : Ship) : Board = {
+  /**
+    * Function to place an horizontal ship on the board
+    *
+    * @param ship : the Ship to add
+    * @return : the new Board with the number of the ship at his coordinates
+    */
+  def add_Ship(ship: Ship): Board = {
         if(ship.orientation){
             //list of the coordinate of the ship
             val shipCoord = List.range(ship.x, ship.x + ship.size())
             //list that will be updated and then placed in the grid
             val tempShipList = List.fill(10)(0)
             //the updated list
-            val shiplist = Create_ship_list_horizontal(tempShipList, shipCoord, ship)
+            val shiplist = create_Ship_List_Horizontal(tempShipList, shipCoord, ship)
             //update the grid with the new list
             val nGrid = this.grid.updated(ship.y, shiplist)
             return Board(nGrid)
         }else{
             val tempShiplist = List.fill(ship.size())(List.fill(10)(0))
             //get the list of list to update the grid
-            val shiplist = Create_ship_list_vertical(tempShiplist, 0, ship)
+            val shiplist = create_Ship_List_Vertical(tempShiplist, 0, ship)
             //create the updated grid
-            val ngrid = Add_vetical_ship_in_grid(grid, shiplist, ship)
+            val ngrid = add_Vertical_Ship_In_Grid(grid, shiplist, ship)
             return Board(ngrid)
         }
-        
-    }
 
-    //Function to create the list of list with the number of the ship at his coordinates
-    def Create_ship_list_vertical(shiplist : List[List[Int]], iterator : Int, ship : Ship) : List[List[Int]] = {
+  }
+
+  /**
+    * Function to create the list of list with the number of the ship at his coordinates
+    *
+    * @param shiplist : the List of List of Int which will be filled with the ship's number where it's necessary
+    * @param iterator : the iterator if
+    * @param ship     : the Ship to place
+    * @return : the List of List of Int to add to the Board grid
+    */
+  def create_Ship_List_Vertical(shiplist: List[List[Int]], iterator: Int, ship: Ship): List[List[Int]] = {
         if(iterator == ship.size()){
             return shiplist
         }else{
             val list = List.fill(10)(0)
             val tempList = list.updated(ship.x, ship.number())
             val nshiplist = shiplist.updated(iterator, tempList)
-            Create_ship_list_vertical(nshiplist, iterator + 1, ship)
+          create_Ship_List_Vertical(nshiplist, iterator + 1, ship)
         }
-    }
+  }
 
-    def Add_vetical_ship_in_grid(grid : List[List[Int]], shiplist : List[List[Int]], ship : Ship) : List[List[Int]] = {
+  /**
+    * Functoin to add a vertical ship to the grid
+    *
+    * @param grid     : a List of List of Int that will be updated with the List from shipList
+    * @param shiplist : a List of List of Int which has the ship's number at y ship coordinates
+    * @param ship     : the Ship to add
+    * @return : return a List of List of Int that will be used to create a new board
+    */
+  def add_Vertical_Ship_In_Grid(grid: List[List[Int]], shiplist: List[List[Int]], ship: Ship): List[List[Int]] = {
         if(shiplist.size == 0){
             return grid
         }else{
             //update the grid with the new line with the last list of the shiplist
             val nGrid = grid.updated(ship.y + shiplist.size-1, shiplist(shiplist.size-1))
             val nshiplist = shiplist.init
-            Add_vetical_ship_in_grid(nGrid, nshiplist, ship)
+          add_Vertical_Ship_In_Grid(nGrid, nshiplist, ship)
         }
-    }
+  }
 
-    //Function to know if a shot as hit a ship or not
-    def Shot_As_Hit(xShot : Int, yShot : Int) : Boolean = {
+  /**
+    * Function to know if a shot as hit a ship or not
+    *
+    * @param xShot : x coordinate of the shot
+    * @param yShot : y coordinate of the shot
+    * @return : true if the shot as hit a ship, otherwise false
+    */
+  def shot_As_Hit(xShot: Int, yShot: Int): Boolean = {
         if(this.grid(yShot)(xShot) > 0) {
             return true
         }else{
             return false
         }
-    }
+  }
 
-    //Funtion to update the grid after a shot with -1 at shot coordinates
-    def Update_Grid_After_Shot(xShot : Int, yShot : Int) : Board = {
+  /**
+    * Function to update the grid after a shot with -1 at shot coordinates
+    *
+    * @param xShot : x coordinate of the shot
+    * @param yShot : y coordinate of the shot
+    * @return : the new Board updated with -1 at the shot coordinates
+    */
+  def update_Grid_After_Shot(xShot: Int, yShot: Int): Board = {
         //create new list to update grid
         val nList = this.grid(yShot).updated(xShot, -1)
         val nGrid = this.grid.updated(yShot, nList)
         return Board(nGrid)
+  }
+
+  /**
+    * Function to know if there still is ship on the board
+    *
+    * @return : true if there still is ship on the board, false otherwise
+    */
+  def as_Ship(): Boolean = {
+    //flat the grid to have one list with all the elements
+    val flattenGrid = this.grid.flatten
+    //filter to get the list of element > 0
+    val as_Ship = flattenGrid.filter(x => x > 0)
+    return !as_Ship.isEmpty
     }
 }
--- battleship/src/test/scala/battleship/BoardSpec.scala
+++ battleship/src/test/scala/battleship/BoardSpec.scala
@@ -25,25 +25,25 @@ class BoardSpec extends FlatSpec with Matchers {
   val shipInVertical = Ship(1,2,false, "Destroyer")
   val shipOutHorizontal = Ship(9,2,true, "Destroyer")
   val shipOutVertical = Ship(2,8,false, "Submarine")
-  "The Board Check_Ship method" should "return true if the boat can be placed on the board" in {
-    board.Check_Ship(shipInHorizontal) shouldEqual true
-    board.Check_Ship(shipInVertical) shouldEqual true
-    board.Check_Ship(shipOutHorizontal) shouldEqual false
-    board.Check_Ship(shipOutVertical) shouldEqual false
-    boardTest.Check_Ship(shipInHorizontal) shouldEqual false
-    boardTest.Check_Ship(shipInVertical) shouldEqual true
+  "The Board check_Ship method" should "return true if the boat can be placed on the board" in {
+    board.check_Ship(shipInHorizontal) shouldEqual true
+    board.check_Ship(shipInVertical) shouldEqual true
+    board.check_Ship(shipOutHorizontal) shouldEqual false
+    board.check_Ship(shipOutVertical) shouldEqual false
+    boardTest.check_Ship(shipInHorizontal) shouldEqual false
+    boardTest.check_Ship(shipInVertical) shouldEqual true
    }
   
   val shipInCoordHorizontal = List.range(shipInHorizontal.x, shipInHorizontal.x + shipInHorizontal.size())
   val shipInCoordVertical = List.range(shipInVertical.y, shipInVertical.y + shipInVertical.size())
   val boardTest2 = Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
   val boardTest3 = Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-  "The Board ShipOverpass method" should "return true if a ship overpass on another" in {
-    boardTest.ShipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual true
-    boardTest2.ShipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual true
-    boardTest3.ShipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual false
-    boardTest2.ShipOverpass(shipInVertical, shipInCoordVertical, false) shouldEqual true
-    boardTest3.ShipOverpass(shipInVertical, shipInCoordVertical, false) shouldEqual false
+  "The Board shipOverpass method" should "return true if a ship overpass on another" in {
+    boardTest.shipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual true
+    boardTest2.shipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual true
+    boardTest3.shipOverpass(shipInHorizontal, shipInCoordHorizontal, false) shouldEqual false
+    boardTest2.shipOverpass(shipInVertical, shipInCoordVertical, false) shouldEqual true
+    boardTest3.shipOverpass(shipInVertical, shipInCoordVertical, false) shouldEqual false
    }
 
   //val test 1
@@ -53,9 +53,9 @@ class BoardSpec extends FlatSpec with Matchers {
   //val test 2 with battleship
   val shiplist2 = List.fill(10)(0)
   val shipCoord2 = List.range(shipInHorizontal.x, shipInHorizontal.x + shipInHorizontal.size())
-  "The Board Create_ship_list_horizontal method" should "return a list with the ship number at the coord of the ship" in {
-    board.Create_ship_list_horizontal(shiplist1, shipCoord1, ship1) shouldEqual List(0,0,0,0,0,5,5,5,5,5)
-    board.Create_ship_list_horizontal(shiplist2, shipCoord2, shipInHorizontal) shouldEqual List(0,4,4,4,4,0,0,0,0,0)
+  "The Board create_Ship_List_Horizontal method" should "return a list with the ship number at the coord of the ship" in {
+    board.create_Ship_List_Horizontal(shiplist1, shipCoord1, ship1) shouldEqual List(0, 0, 0, 0, 0, 5, 5, 5, 5, 5)
+    board.create_Ship_List_Horizontal(shiplist2, shipCoord2, shipInHorizontal) shouldEqual List(0, 4, 4, 4, 4, 0, 0, 0, 0, 0)
    }
 
   //val test 1
@@ -64,36 +64,42 @@ class BoardSpec extends FlatSpec with Matchers {
   //val test 2
   val shipVertical2 = Ship(3,5,false, "Destroyer")
   val shiplistVertical2 = List.fill(shipVertical2.size())(List.fill(10)(0))
-  "The Board Create_ship_list_vertical method" should "return a list of list with the new list of coordinates" in {
-    board.Create_ship_list_vertical(shiplistVertical1, 0, shipVertical1) shouldEqual List(List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0))
-    board.Create_ship_list_vertical(shiplistVertical2, 0, shipVertical2) shouldEqual List(List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0))
+  "The Board create_Ship_List_Vertical method" should "return a list of list with the new list of coordinates" in {
+    board.create_Ship_List_Vertical(shiplistVertical1, 0, shipVertical1) shouldEqual List(List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0))
+    board.create_Ship_List_Vertical(shiplistVertical2, 0, shipVertical2) shouldEqual List(List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0))
    }
 
   val shiplistVertical1done = List(List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0))
   val shiplistVertical2done = List(List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0))
-  "The Board Add_vetical_ship_in_grid method" should "return a list of list with the new list of coordinates" in {
-    board.Add_vetical_ship_in_grid(firstGrid ,shiplistVertical1done, shipVertical1) shouldEqual List(List(0,0,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0))
-    board.Add_vetical_ship_in_grid(firstGrid ,shiplistVertical2done, shipVertical2) shouldEqual List(List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0))
+  "The Board add_Vertical_Ship_In_Grid method" should "return a list of list with the new list of coordinates" in {
+    board.add_Vertical_Ship_In_Grid(firstGrid, shiplistVertical1done, shipVertical1) shouldEqual List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    board.add_Vertical_Ship_In_Grid(firstGrid, shiplistVertical2done, shipVertical2) shouldEqual List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
    }
 
-  "The Board Add_ship method" should "return a list of list with the new list of coordinates" in {
-    board.Add_ship(shipVertical1) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,2,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    board.Add_ship(shipVertical2) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,1,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    board.Add_ship(ship1) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,5,5,5,5,5),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    board.Add_ship(shipInHorizontal) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,4,4,4,4,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
+  "The Board add_Ship method" should "return a list of list with the new list of coordinates" in {
+    board.add_Ship(shipVertical1) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 2, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    board.add_Ship(shipVertical2) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    board.add_Ship(ship1) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 5, 5, 5, 5, 5), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    board.add_Ship(shipInHorizontal) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 4, 4, 4, 4, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
    }
 
-  "The Board Shot_As_Hit method" should "return true if there is a ship where we have shot" in {
-    board.Shot_As_Hit(5, 3) shouldEqual false
-    boardTest.Shot_As_Hit(4, 8) shouldEqual false
-    boardTest.Shot_As_Hit(1,1) shouldEqual true
-    boardTest2.Shot_As_Hit(1,2) shouldEqual true
+  "The Board shot_As_Hit method" should "return true if there is a ship where we have shot" in {
+    board.shot_As_Hit(5, 3) shouldEqual false
+    boardTest.shot_As_Hit(4, 8) shouldEqual false
+    boardTest.shot_As_Hit(1, 1) shouldEqual true
+    boardTest2.shot_As_Hit(1, 2) shouldEqual true
    }
 
-  "The Board Update_Grid_After_Shot method" should "return the new grid with -1 where we have shot" in {
-    board.Update_Grid_After_Shot(5,3) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,-1,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    boardTest.Update_Grid_After_Shot(4,8) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,4,4,4,4,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,-1,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    boardTest.Update_Grid_After_Shot(1,1) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,-1,4,4,4,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
-    boardTest2.Update_Grid_After_Shot(1,2) shouldEqual Board(List(List(0,0,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,-1,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,4,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0)))
+  "The Board update_Grid_After_Shot method" should "return the new grid with -1 where we have shot" in {
+    board.update_Grid_After_Shot(5, 3) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, -1, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    boardTest.update_Grid_After_Shot(4, 8) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 4, 4, 4, 4, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, -1, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    boardTest.update_Grid_After_Shot(1, 1) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, -1, 4, 4, 4, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
+    boardTest2.update_Grid_After_Shot(1, 2) shouldEqual Board(List(List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 4, 0, 0, 0, 0, 0, 0, 0, 0), List(0, -1, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 4, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 4, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)))
    }
+
+  "The Board as_Ship method" should "return false if there is no ship in the board, true otherwise" in {
+    board.as_Ship() shouldEqual false
+    boardTest.as_Ship() shouldEqual true
+    boardTest2.as_Ship() shouldEqual true
+  }
 }
